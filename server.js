c2cb46d1fb5f3934697ada1f78f7992dcbe1fde8
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-01-10 21:40:16 +0100

    Add eslint and fix linting errors.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require(`path`);
 const zlib = require(`zlib`);
 
 const publicDirectory = path.join(__dirname, `public`);
+// eslint-disable-next-line import/no-dynamic-require
 const manifest = require(`${publicDirectory}/manifest.json`);
 
 const contentTypes = {
@@ -13,29 +14,32 @@ const contentTypes = {
 
 const onRequest = (request, response) => {
   const filename = path.join(publicDirectory, request.url).split(`.no-push`).join(``);
-  const acceptEncoding = request.headers['accept-encoding'] || ``;
-  const push = request.url.split(`.no-push`).length > 1 ? false : true;
+  const pushEnabled = request.url.indexOf(`.no-push`) === -1;
 
   if (request.url.includes(`.html`)) {
     const pushFiles = manifest[request.url] ? Object.keys(manifest[request.url]) : [];
-    if (push && response.push && pushFiles.length) {
+    if (pushEnabled && response.push && pushFiles.length) {
       pushFiles.forEach((file) => {
         const contentType = contentTypes[manifest[request.url][file].type] || `text/html`;
         const push = response.push(file);
-        push.writeHead(200, { 'Content-Encoding': 'deflate', 'Content-Type': contentType });
+        push.writeHead(200, { 'Content-Encoding': `deflate`, 'Content-Type': contentType });
         fs.createReadStream(path.join(publicDirectory, file)).pipe(zlib.createDeflate()).pipe(push);
       });
     }
     const fileStream = fs.createReadStream(filename);
-    response.writeHead(200, { 'Content-Encoding': 'deflate' });
+    response.writeHead(200, { 'Content-Encoding': `deflate` });
     fileStream.pipe(zlib.createDeflate()).pipe(response);
-    fileStream.on('finish', response.end);
-  } else if ((filename.indexOf(__dirname) === 0) && fs.existsSync(filename) && fs.statSync(filename).isFile()) {
+    fileStream.on(`finish`, response.end);
+  } else if (
+    (filename.indexOf(__dirname) === 0) &&
+    fs.existsSync(filename) &&
+    fs.statSync(filename).isFile()
+  ) {
     // Serve static files.
     const fileStream = fs.createReadStream(filename);
-    response.writeHead(200, { 'Content-Encoding': 'deflate' });
+    response.writeHead(200, { 'Content-Encoding': `deflate` });
     fileStream.pipe(zlib.createDeflate()).pipe(response);
-    fileStream.on('finish', response.end);
+    fileStream.on(`finish`, response.end);
   } else {
     response.writeHead(404);
     response.end(request.url);
@@ -43,6 +47,6 @@ const onRequest = (request, response) => {
 };
 
 http2.createServer({
-  key: fs.readFileSync(path.join(__dirname, '/certs/key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '/certs/cert.pem'))
+  key: fs.readFileSync(path.join(__dirname, `/certs/key.pem`)),
+  cert: fs.readFileSync(path.join(__dirname, `/certs/cert.pem`)),
 }, onRequest).listen(8080);
