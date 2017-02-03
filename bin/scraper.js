8820ba57869cff5a41292dc7a95f778fc7738f43
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-03 20:02:16 +0100

    Create empty manifest file in the sites directory.
    The server needs a manifest.json file to start.

--- bin/scraper.js
+++ bin/scraper.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 
 const args = require(`args`);
+const fs = require(`fs`);
 const path = require(`path`);
 const scrape = require(`website-scraper`);
 
 args.option(`url`, `The URL which should be scrapped.`);
 const flags = args.parse(process.argv);
 const directory = path.resolve(__dirname, `../sites/${flags.url.split(`://`)[1]}`);
+const manifestPath = path.resolve(directory, `manifest.json`);
 
 scrape({
   urls: [flags.url],
   directory,
 }).then(() => {
+  fs.writeFileSync(manifestPath, `{}`);
   // eslint-disable-next-line no-console
   console.log(`Successfully scraped: ${flags.url}`);
 });
