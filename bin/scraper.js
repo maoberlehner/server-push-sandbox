33fe66c983c5fdc3f85285fab2c6b1d3baf22b8d
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-03 20:22:14 +0100

    Add scrapped CSS and JS files to the manifest.json
    By default all CSS and JS files are added to the manifest.json file which defines which resources should get server pushed.

--- bin/scraper.js
+++ bin/scraper.js
@@ -2,6 +2,7 @@
 
 const args = require(`args`);
 const fs = require(`fs`);
+const glob = require(`glob`);
 const path = require(`path`);
 const scrape = require(`website-scraper`);
 
@@ -9,12 +10,34 @@ args.option(`url`, `The URL which should be scrapped.`);
 const flags = args.parse(process.argv);
 const directory = path.resolve(__dirname, `../sites/${flags.url.split(`://`)[1]}`);
 const manifestPath = path.resolve(directory, `manifest.json`);
+const manifest = {
+  '/index.html': {},
+};
 
 scrape({
   urls: [flags.url],
   directory,
 }).then(() => {
-  fs.writeFileSync(manifestPath, `{}`);
+  const cssFiles = glob.sync(path.resolve(directory, `**/*.css`));
+  const jsFiles = glob.sync(path.resolve(directory, `**/*.js`));
+
+  cssFiles.forEach((cssFile) => {
+    const relativePath = path.resolve(`/`, path.relative(directory, cssFile));
+    manifest[`index.html`][relativePath] = {
+      type: `style`,
+      weight: `1`,
+    };
+  });
+
+  jsFiles.forEach((jsFile) => {
+    const relativePath = path.resolve(`/`, path.relative(directory, jsFile));
+    manifest[`index.html`][relativePath] = {
+      type: `script`,
+      weight: `1`,
+    };
+  });
+
+  fs.writeFileSync(manifestPath, JSON.stringify(manifest));
   // eslint-disable-next-line no-console
   console.log(`Successfully scraped: ${flags.url}`);
 });
