9d92f3a7664a6ea4b65056e042f28d81a31a76db
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-04 08:00:33 +0100

    Log a comparision chart after the benchmark.

--- bin/benchmark.js
+++ bin/benchmark.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 const args = require(`args`);
+const chalk = require(`chalk`);
 const exec = require(`child_process`).exec;
 const fs = require(`fs`);
 const path = require(`path`);
+const Table = require(`cli-table`);
 
 args.option(
   `connection`,
@@ -26,6 +28,7 @@ const browsertimeCommand = [
 ];
 const pushUrl = `-u https://127.0.0.1:8080/index.html`;
 const noPushUrl = `-u https://127.0.0.1:8080/index.no-push.html`;
+const data = { push: [], noPush: [] };
 
 function executeBrosertime(url) {
   return new Promise((resolve) => {
@@ -36,11 +39,71 @@ function executeBrosertime(url) {
         __dirname,
         `../log/result.json`
       ));
-      console.log(`NO-PUSH: `, JSON.parse(benchmarkFile).default.statistics.domContentLoadedTime.median);
-      resolve(JSON.parse(benchmarkFile).default.statistics.domContentLoadedTime.median);
+      resolve(JSON.parse(benchmarkFile).default.statistics);
     });
   });
 }
 
+function formatValue(mainValue, compareValue) {
+  if (!compareValue) return mainValue;
+  if (mainValue < compareValue) return chalk.green.bold(mainValue);
+  return mainValue;
+}
+
+function logResult(statisticData) {
+  const pushData = statisticData.push;
+  const noPushData = statisticData.noPush;
+  const resultTable = new Table({
+    head: [
+      ``,
+      chalk.reset.bold(`push`),
+      chalk.reset.bold(`no-push`),
+    ],
+  });
+
+  const firstPaint = {};
+  firstPaint[chalk.reset.bold(`firstPaint`)] = [
+    formatValue(pushData.firstPaint.median, noPushData.firstPaint.median),
+    formatValue(noPushData.firstPaint.median, pushData.firstPaint.median),
+  ];
+  resultTable.push(firstPaint);
+
+  const domContentLoadedTime = {};
+  domContentLoadedTime[chalk.reset.bold(`domContentLoadedTime`)] = [
+    formatValue(pushData.domContentLoadedTime.median, noPushData.domContentLoadedTime.median),
+    formatValue(noPushData.domContentLoadedTime.median, pushData.domContentLoadedTime.median),
+  ];
+  resultTable.push(domContentLoadedTime);
+
+  const domInteractiveTime = {};
+  domInteractiveTime[chalk.reset.bold(`domInteractiveTime`)] = [
+    formatValue(pushData.domInteractiveTime.median, noPushData.domInteractiveTime.median),
+    formatValue(noPushData.domInteractiveTime.median, pushData.domInteractiveTime.median),
+  ];
+  resultTable.push(domInteractiveTime);
+
+  const pageLoadTime = {};
+  pageLoadTime[chalk.reset.bold(`pageLoadTime`)] = [
+    formatValue(pushData.pageLoadTime.median, noPushData.pageLoadTime.median),
+    formatValue(noPushData.pageLoadTime.median, pushData.pageLoadTime.median),
+  ];
+  resultTable.push(pageLoadTime);
+
+  const speedIndex = {};
+  speedIndex[chalk.reset.bold(`speedIndex`)] = [
+    formatValue(pushData.speedIndex.median, noPushData.speedIndex.median),
+    formatValue(noPushData.speedIndex.median, pushData.speedIndex.median),
+  ];
+  resultTable.push(speedIndex);
+  console.log(resultTable.toString());
+}
+
 executeBrosertime(pushUrl)
-  .then(() => executeBrosertime(noPushUrl));
+  .then((statisticData) => {
+    data.push = statisticData;
+    return executeBrosertime(noPushUrl);
+  })
+  .then((statisticData) => {
+    data.noPush = statisticData;
+    logResult(data);
+  });
